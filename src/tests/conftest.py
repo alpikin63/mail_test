b1fe8f48832ed7b8cf303c0a753d126fb79f7e06
Alexandr Pikin <pikin@aeroidea.ru>
2019-11-20 14:29:51 +0300

    commit

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,10 +2,12 @@ import pytest
 from src.model.user import User
 from src.model.mail import Mail
 
+
 @pytest.fixture()
 def data_users():
     tru_user = User(login='testt2005', password='123Qa!', domain=1)
     return tru_user
+
 
 @pytest.fixture()
 def data_mails():
